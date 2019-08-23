5fa2ef1764e9c597d17bdb8d471faacf26ec1719
abhilash <abhilashdas472@gmail.com>
2019-08-23 17:55:42 +0530

    first commit

---
+++ resources/js/components/ChatMessage.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-8">
+                <div class="card">
+                <p>{{message}}</p>
+                <small>{{author}}</small>
+                </div>
+          </div>
+          </div>
+        </div>
+
+</template>
+
+<script>
+    export default {
+      data() {
+
+          return {
+              message : "this is your message",
+              author : "abhilash das"
+          }
+
+      }
+    }
+</script>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section("content")
+
+   <h1>Chat Room</h1>
+
+   <chat-message></chat-message>
+
+     <chat-log></chat-log>
+
+          <chat-composer></chat-composer>
+
+
+
+
+
+@stop
